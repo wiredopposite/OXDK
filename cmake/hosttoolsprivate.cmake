80d5338b72b3f31ece72848901ef82065011f0dd
wiredopposite <jackxhorizon@gmail.com>
2026-07-11 13:42:19 -0600

    resolve errors using ninja, add warning supression in extract-xiso build

--- cmake/hosttoolsprivate.cmake
+++ cmake/hosttoolsprivate.cmake
@@ -13,14 +13,19 @@ function(OxdkEnsureCxbe TARGET_NAME OUT_EXE)
         return()
     endif()
 
-    set(_cxbe_bin "$ENV{OXDK_DIR}/tools/cxbe/cxbe")
+    set(_cxbe_build_dir "$ENV{OXDK_DIR}/tools/cxbe/build")
+    set(_cxbe_bin "${_cxbe_build_dir}/cxbe")
+
     if(NOT TARGET oxdk_cxbe_build)
         add_custom_command(
             OUTPUT  "${_cxbe_bin}"
-            COMMAND ${CMAKE_MAKE_PROGRAM} -C "$ENV{OXDK_DIR}/tools/cxbe"
+            COMMAND ${CMAKE_COMMAND} -S "$ENV{OXDK_DIR}/tools/cxbe" -B "${_cxbe_build_dir}" -G "${CMAKE_GENERATOR}" -DCMAKE_BUILD_TYPE=Release
+            COMMAND ${CMAKE_COMMAND} --build "${_cxbe_build_dir}" --config Release
             COMMENT "Building cxbe..."
+            VERBATIM
         )
         add_custom_target(oxdk_cxbe_build DEPENDS "${_cxbe_bin}")
+        set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${_cxbe_build_dir}")
     endif()
 
     set(OXDK_CXBE "${_cxbe_bin}" CACHE FILEPATH "Path to cxbe" FORCE)
@@ -45,14 +50,27 @@ function(OxdkEnsureExtractXiso TARGET_NAME OUT_EXE)
 
     set(_extract_xiso_build_dir "$ENV{OXDK_DIR}/tools/extract-xiso/build")
     set(_extract_xiso_bin "${_extract_xiso_build_dir}/extract-xiso")
+
+    set(_extract_xiso_c_flags "")
+    if(CMAKE_HOST_WIN32)
+        set(_extract_xiso_c_flags "-DWIN32 -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE")
+    endif()
+
     if(NOT TARGET oxdk_extract_xiso_build)
         add_custom_command(
             OUTPUT  "${_extract_xiso_bin}"
-            COMMAND ${CMAKE_COMMAND} -S "$ENV{OXDK_DIR}/tools/extract-xiso" -B "${_extract_xiso_build_dir}" -G "${CMAKE_GENERATOR}" -DCMAKE_BUILD_TYPE=Release
+            COMMAND ${CMAKE_COMMAND} 
+                -S "$ENV{OXDK_DIR}/tools/extract-xiso" 
+                -B "${_extract_xiso_build_dir}" 
+                -G "${CMAKE_GENERATOR}" 
+                -DCMAKE_BUILD_TYPE=Release 
+                "-DCMAKE_C_FLAGS=${_extract_xiso_c_flags}"
             COMMAND ${CMAKE_COMMAND} --build "${_extract_xiso_build_dir}" --config Release
             COMMENT "Building extract-xiso..."
+            VERBATIM
         )
         add_custom_target(oxdk_extract_xiso_build DEPENDS "${_extract_xiso_bin}")
+        set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${_extract_xiso_build_dir}")
     endif()
 
     set(OXDK_EXTRACT_XISO "${_extract_xiso_bin}" CACHE FILEPATH "Path to extract-xiso" FORCE)
